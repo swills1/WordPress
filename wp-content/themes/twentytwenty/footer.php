ba75b0ec0cebb34c571ebad8ab74d48a08d19e8a
steven@swills.me <swills@CERASIS.NET>
2020-08-31 10:55:52 -0500

    test commit 1

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -30,7 +30,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by WordPress and GTZ!', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
